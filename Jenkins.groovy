735a5da0902f6e020673a1a08901b7705ff8fbae
pavanpalveproject <125024203+pavanpalveproject@users.noreply.github.com>
2023-03-07 19:46:06 +0530

    sada

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -11,7 +11,10 @@ pipeline {
                 script(){
                     checkout scm
                     // branch = env.BRANCH_NAME
-                    sh 'echo $env.BRANCH_NAME'
+                    // sh 'echo $env.BRANCH_NAME'
+                    if(env.BRANCH_NAME == 'master'){
+                        sh 'echo psp'
+                    }
                 }
             }
         }
