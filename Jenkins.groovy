b79227826ea54442a88c01c16d772da9c3d20292
pavanpalveproject <125024203+pavanpalveproject@users.noreply.github.com>
2023-03-07 19:27:08 +0530

    aasds

---
+++ Jenkins.groovy
@@ -0,0 +1,19 @@
+def base_path = "gcp-op-as-infra-saas"
+
+pipeline {
+  agent any
+
+  environment{
+        branch = "$params.Env"
+    }
+
+    stages{
+        stage(test){
+            steps{
+                script(){
+                    sh 'echo branch'
+                }
+            }
+        }
+    }
+}
