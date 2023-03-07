6af11905a3e04740d26dda292457aa85fa461309
pavanpalveproject <125024203+pavanpalveproject@users.noreply.github.com>
2023-03-07 20:08:28 +0530

    sfdf

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -6,12 +6,20 @@ pipeline {
   
 
     stages{
+    stage('Checkout') {
+        steps{
+            checkout([$class: 'GitSCM',
+            branches: [[name: '!master']],
+            userRemoteConfigs: [[url: 'https://github.com/pavanpalveproject/testing']]])
+        }
+    }
+
         stage(test){
             steps{
                 script(){
-                    checkout scm
-                    // branch = env.BRANCH_NAME
-                    // sh 'echo $env.BRANCH_NAME'
+                    
+                    branch = env.BRANCH_NAME
+                    sh 'echo $env.BRANCH_NAME'
                     if(env.BRANCH_NAME == 'master'){
                         sh 'echo psp'
                     }
