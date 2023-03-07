01fbe60a677af1d965a03cc1748c803bf46f7817
pavanpalveproject <125024203+pavanpalveproject@users.noreply.github.com>
2023-03-07 19:30:51 +0530

    zxcz

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -11,7 +11,7 @@ pipeline {
         stage(test){
             steps{
                 script(){
-                    sh 'echo branch'
+                    sh 'echo ${branch}'
                 }
             }
         }
